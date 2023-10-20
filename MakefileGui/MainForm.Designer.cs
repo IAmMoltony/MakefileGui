70be2e8a8d5da643c9da8340d7af6698e74c418c
IAmMoltony <koronavirusnyj@gmail.com>
2023-10-21 01:40:35 +0600

    Make windows not resizable

--- MakefileGui/MainForm.Designer.cs
+++ MakefileGui/MainForm.Designer.cs
@@ -319,6 +319,7 @@
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.menuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip;
             this.Name = "MainForm";
